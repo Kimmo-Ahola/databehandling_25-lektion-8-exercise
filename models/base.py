3ea485ad60d6e7cf480b5f9e9156a5ef707bc6a2
kahol <kahola@gmail.com>
2025-11-22 21:57:09 +0100

    added naming conventions to base class

--- models/base.py
+++ models/base.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import MappedAsDataclass, DeclarativeBase, mapped_column, Mapped, relationship
-from sqlalchemy import String, Integer
+from sqlalchemy import MetaData, String, Integer
 from typing import Annotated
 
 
@@ -7,4 +7,10 @@ str_255 = Annotated[str, mapped_column(String(255))]
 int_pk = Annotated[int, mapped_column(Integer, primary_key=True, autoincrement=True)]
 
 class Base(MappedAsDataclass, DeclarativeBase):
-    pass
+    metadata = MetaData(naming_convention={
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s",
+    })
