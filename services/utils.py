17871391daf8224193168cd495816e3e957c19b1
kahol <kahola@gmail.com>
2025-11-23 10:31:09 +0100

    fix utils nullability

--- services/utils.py
+++ services/utils.py
@@ -1,6 +1,6 @@
 class Utils():
     @staticmethod
-    def split_full_name(full_name: str) -> tuple[str, str | None]:
+    def split_full_name(full_name: str) -> tuple[str | None, str | None]:
         parts = full_name.split()
         first_name = parts[0] if len(parts) > 0 else ""
         last_name = parts[1] if len(parts) > 1 else None
