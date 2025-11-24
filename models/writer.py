d766f693f4845d8fa01c5fd8e65e14bbed504cd9
Kimmo Ahola @ Systementor <kimmo.ahola@systementor.se>
2025-11-24 08:29:56 +0100

    Update writer.py

--- models/writer.py
+++ models/writer.py
@@ -10,6 +10,7 @@ class Writer(Base):
     first_name: Mapped[str_255]
     last_name: Mapped[Optional[str_255]]
 
+    # https://dev.to/arctype/a-complete-guide-to-generated-columns-in-mysql-2lnb
     full_name: Mapped[str] = mapped_column(String(1000),
         Computed("CONCAT(IFNULL(first_name, ''), ' ', IFNULL(last_name, ''))"),
         init=False,
