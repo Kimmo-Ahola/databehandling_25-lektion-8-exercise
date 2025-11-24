f08850ba599388011062a9f4df07b77c25e9c402
Kimmo Ahola @ Systementor <kimmo.ahola@systementor.se>
2025-11-24 08:29:22 +0100

    Update actor.py

--- models/actor.py
+++ models/actor.py
@@ -9,6 +9,7 @@ class Actor(Base):
     first_name: Mapped[str_255]
     last_name: Mapped[Optional[str_255]]
 
+    # https://dev.to/arctype/a-complete-guide-to-generated-columns-in-mysql-2lnb
     full_name: Mapped[str] = mapped_column(String(1000),
         Computed("CONCAT(IFNULL(first_name, ''), ' ', IFNULL(last_name, ''))"),
         init=False,
