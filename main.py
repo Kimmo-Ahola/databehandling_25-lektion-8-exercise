981fca3520770abbb9c4b71dabda6d0ce0b68d80
Kimmo <kimmo.ahola@systementor.se>
2025-11-26 14:36:19 +0100

    added example to main file

--- main.py
+++ main.py
@@ -1,8 +1,31 @@
+from database.db import My_Session
+from models.actor import Actor
 from services.seeding import Seeding
-# TODO add stuff here
-# Need to figure out some exercises
-# create a menu?
+from sqlalchemy.orm import Session
+
+"""
+Check the readme.md file for exercises
+
+You can remove 
+    Base.metadata.drop_all(engine)
+    Base.metadata.create_all(engine)
+from the seeding class and use alembic if you want.
+
+Write the questions as functions like below (or create a console menu)
+"""
+
+def Q_1(session: Session) -> Actor | None:
+    query = session.query(Actor)
+
+    res = query.first()
+
+    return res
 
 
 if __name__ == '__main__':
     Seeding.create_movie_database()
+    
+    with My_Session() as session:
+        res = Q_1(session)
+        if res:
+            print(res.full_name)
